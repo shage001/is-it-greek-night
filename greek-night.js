53f0c24f40c726dbb9d436986b29edf13c41be8f
Samuel Hage <shage@basin.cs.middlebury.edu>
2015-11-18 19:19:06 -0500

    change everything so the app actually works

--- greek-night.js
+++ greek-night.js
@@ -1,73 +1,150 @@
 /**
- * Sam Hage
  * Is it greek night?
+ * Sam Hage
  * 11/2015
- */
-
-readFile( 'today.txt', isItGreekNight );
-
-/**********************************************************************************************************************
- * Functions to run on AJAX success and failure
- */
-function xhrSuccess() { this.callback.apply( this ); }
-
-function xhrError () { console.error( this.statusText ); }
-
-
-/**********************************************************************************************************************
- * Try to load the file provided
  *
- * @param: {string} sURL Path to the file
- * @param: {function} fCallback Callback function
+ * TODO:
+ * - list next night
+ * - email list
  */
-function readFile ( sURL, fCallback )
+
+/* get the menu JSON and change HTML if appropriate */
+window.addEventListener( 'load', function()
 {
-	/* create a request and set its callback */
-	var request = new XMLHttpRequest();
-	request.callback = fCallback;
-	request.onload = xhrSuccess;
-	request.onerror = xhrError;
-
-	/* open the request and send */
-	request.open( 'GET', sURL, true );
-	request.send( null );
-}
-
-
-/**********************************************************************************************************************
- * Determine if it's greek night based on the contents of today.txt
- */
-function isItGreekNight()
-{
-	var isGreek = ( this.responseText.toLowerCase().includes( 'spanakopita' ) && 
-				  this.responseText.toLowerCase().includes( 'tzatziki' ) ) ||
-				( this.responseText.toLowerCase().includes( 'lamb' ) &&
-				  this.responseText.toLowerCase().includes( 'pita' ) &&
-				  this.responseText.toLowerCase().includes( 'falafel' ) );
-	console.log( isGreek );
+	tomorrow = formatDate( 1 );
+	yesterday = formatDate( -1 );
 
 	var response = document.getElementsByClassName( 'response' )[0];
-	if ( isGreek ) {
-		var d = new Date();
-		if ( d.getHours < 20 ) { // make sure the dining hall is still open
-			response.innerHTML = 'Yes! Hurry up and get to Ross!';
+	response.innerHTML = 'Nope! Sorry.'
+
+	getJSON( 'http://middmenuapi.herokuapp.com/' ).then( function( response )
+	{
+		if ( greekNight( response ) ) {
+			var response = document.getElementsByClassName( 'response' )[0];
+			response.innerHTML = 'Yes! Get over to Ross right now!'
 		}
-		else {
-			response.ineerHTML = 'You just missed it!';
+	}).catch( function( error )
+	{
+		console.log( error.message );
+	});
+
+	getJSON( 'http://middmenuapi.herokuapp.com/' + yesterday ).then( function( response )
+	{
+		if ( greekNight( response ) ) {
+			var response = document.getElementsByClassName( 'response' )[0];
+			response.innerHTML = 'Nope, it was yesterday.'
 		}
-	}
-	else {
-		response.innerHTML = 'Nope! Sorry.';
-	}
+	}).catch( function( error )
+	{
+		console.log( error.message );
+	});
+
+	getJSON( 'http://middmenuapi.herokuapp.com/' + tomorrow ).then( function( response )
+	{
+		if( greekNight( response ) ) {
+			var response = document.getElementsByClassName( 'response' )[0];
+			response.innerHTML = 'Nope...but it is tomorrow!'
+		}
+	}).catch( function( error )
+	{
+		console.log( error.message );
+	});
+});
+
+
+/**********************************************************************************************************************
+ * Determine if it is greek night based on the given menu. This is a little
+ * experimental because the menus aren't perfectly consistent
+ *
+ * @param: {string} response The entire menu
+ * @return: {boolean} Whether or not it is greek night
+ */
+var greekNight = function( response )
+{
+	return response.toLowerCase().includes( 'tzatziki' ) &&
+		   response.toLowerCase().includes( 'lamb' ) &&
+		   response.toLowerCase().includes( 'pita' ) ||
+		   response.toLowerCase().includes( 'gyros' );
 }
 
 
+/**********************************************************************************************************************
+ * Format the date to yyyy-mm-dd
+ *
+ * @param: {number} offset The number of days off from today e.g. -1 for
+ * yesterday, 1 for tomorrow
+ * @return: {string} The date
+ */
+var formatDate = function( offset )
+{
+	var d = new Date();
+	d.setDate( d.getDate() + offset );
+
+	var dd = d.getDate();
+	var mm = d.getMonth() + 1;
+	var yyyy = d.getFullYear();
+
+	if( dd < 10 ) {
+	    dd = '0' + dd;
+	}
+	if( mm < 10 ) {
+	    mm = '0' + mm;
+	}
+
+	return yyyy + '-' + mm + '-' + dd;
+}
+
+/**********************************************************************************************************************
+ * Get a text representation of the dining menus on the given date. If no date
+ * is given it will give today's.
+ *
+ * @param: date The desired date in yyyy-mm-dd form
+ * @return: The menu text
+ */
+var getMenuOn = function( date )
+{
+	date = date || '';
+	var url = 'http://middmenuapi.herokuapp.com/' + date;
+	getJSON( url ).then( function( response )
+	{
+		return response;
+	}).catch( function( error )
+	{
+		console.log( error.message );
+	});
+};
 
 
-
-
-
-
-
-
-
+/**********************************************************************************************************************
+ * Create and issue an HTTP request for JSON data
+ *
+ * @param: {string} url Location of the information
+ * @return: {Promise}
+ */
+var getJSON = function( url )
+{
+	return new Promise( function( resolve, reject )
+	{
+		/* create a new request */
+		var request = new XMLHttpRequest();
+		/* event listener for success */
+		request.addEventListener( 'load', function()
+		{
+			if ( request.status >= 200 && request.status < 400 ) {
+				resolve( request.response );
+			}
+			else {
+				reject( Error( request.statusText ) );
+			}
+		});
+		/* event listener for error */
+		request.addEventListener( 'error', function()
+		{
+			reject( Error( 'Network error' ) );
+		});
+		/* open the request then send it */
+		request.responType = 'json';
+		request.open( 'GET', url, true );
+		request.send();
+	});
+};
--- scrape-menu.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python scrape-menu.py
